723ac262a3ad3e1934eb24d73f0802f3f94c6e86
LAPTOP-Q88R703D\driwo <dries.wouters1@student.kuleuven.be>
2022-03-09 15:29:52 +0100

    initiele setup

--- model/Difficulty.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Difficulty {
-    EASY, MEDIUM, HARD
-}
--- notifier/ITileStateNotifier.java
+++
@@ -1,8 +0,0 @@
-package notifier;
-
-public interface ITileStateNotifier {
-    void notifyOpened(int explosiveNeighbourCount);
-    void notifyFlagged();
-    void notifyUnflagged();
-    void notifyExplode();
-}
