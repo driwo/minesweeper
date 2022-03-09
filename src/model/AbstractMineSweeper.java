2df9b5fd55b46366377d675e42284145686cc5ea
LAPTOP-1KIITMR9\vango <ruben.vangorp@student.kuleuven.be>
2022-03-09 15:55:13 +0100

    Het werkt nog niet goed

--- src/model/AbstractMineSweeper.java
+++ src/model/AbstractMineSweeper.java
@@ -1,9 +1,12 @@
 package model;
-
+//Hoi
 import notifier.IGameStateNotifier;
 import test.TestableMinesweeper;
 
+
+
 public abstract class AbstractMineSweeper implements TestableMinesweeper {
+
     protected IGameStateNotifier viewNotifier;
     public final void setGameStateNotifier(IGameStateNotifier notifier){
         this.viewNotifier = notifier;
