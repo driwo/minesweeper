12e5e557b5db06215e05842698b0118303ef17b4
LAPTOP-Q88R703D\driwo <dries.wouters1@student.kuleuven.be>
2022-03-23 09:43:06 +0100

    is zien

--- src/model/Minesweeper.java
+++ src/model/Minesweeper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-
+//doe je best he gorpman
 
 public class Minesweeper extends AbstractMineSweeper implements TestableMinesweeper
 {
